283377451bb6a647da22bd2c20761a4321aaac14
Yoon Young <yunyoung1819@hanmail.net>
2018-01-24 02:54:19 +0900

    게시물 삭제처리

--- src/main/java/com/ese/controller/SearchBoardController.java
+++ src/main/java/com/ese/controller/SearchBoardController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ese.domain.PageMaker;
 import com.ese.domain.SearchCriteria;
@@ -34,15 +36,47 @@ public class SearchBoardController {
 		
 		logger.info(cri.toString());
 		
-		//model.addAttribute("list", service.listCriteria(cri));
 		model.addAttribute("list", service.listSearchCriteria(cri));
 		
 		PageMaker pageMaker = new PageMaker();
 		
 		pageMaker.setCri(cri);
-		//pageMaker.setTotalCount(service.listCountCriteria(cri));
 		pageMaker.setTotalCount(service.listSearchCount(cri));
 		
 		model.addAttribute("pageMaker", pageMaker);
 	}
+	
+	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, 
+			@ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception{
+		
+		model.addAttribute(service.read(bno));
+	}
+	
+	// 게시물 삭제 처리
+	@RequestMapping(value = "/removePage", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno,
+			SearchCriteria cri,
+			RedirectAttributes rttr) throws Exception{
+		
+		service.remove(bno);
+		
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addAttribute("searchType", cri.getSearchType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+		
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/sboard/list";
+	}
+	
+	//게시물 수정 처리
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
+	public void modifyPagingGET(int bno, @ModelAttribute("cri") SearchCriteria cri,
+			Model model) throws Exception{
+		
+		model.addAttribute(service.read(bno));
+	}
+	
 }
